3cb386fe1fdc76063e879eccc814921c12b6e8b4
AngelMaharjan <100304509+AngelMaharjan@users.noreply.github.com>
2022-10-11 22:59:40 +0545

    My part of code

--- linkedlist/ReorderList.java
+++ linkedlist/ReorderList.java
@@ -2,13 +2,86 @@
 package linked_list;
 
 public class ReorderList {
+	
+	Node head = null;
+	
+	class Node{
+		int data = 0;
+		Node next = null;
+		
+		Node(int data){
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	public void reorderLists(Node head) {
+		Node slow = head;
+		Node fast = head.next;
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		
+		Node second = slow.next;
+		Node prev = slow.next = null;
+		while(second != null) {
+			Node tmp = second.next;
+			second.next = prev;
+			prev = second;
+			second = tmp;
+		}
+		
+		Node first = head;
+		second = prev;
+		while(second != null) {
+			Node tmp1 = first.next;
+			Node tmp2 = second.next;
+			first.next = second;
+			second.next = tmp1;
+			first = tmp1;
+			second = tmp2;
+		}
+	
+    }
+		
+	
+	public void arrayToNode(int[] arr) {
+		Node node = null;
+		for(int i = arr.length -1; i >= 0; i--) {
+			node = new Node(arr[i]);
+			if(head == null) {
+				head = node;
+			}
+			else {
+				node.next = head;
+				head = node;
+			}
+		}	
+	}
+	
+	public void printNode() {
+		Node node = head;
+		while(node != null) {
+			System.out.print(node.data + " -> ");
+			node = node.next;
+		}
+		System.out.println("null");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] array = {1, 2, 3, 4, 5};
+		ReorderList rl = new ReorderList(); 
+		rl.arrayToNode(array);
+		rl.printNode();
+		System.out.println("After reordering lists");
+		rl.reorderLists(rl.head);
+		rl.printNode();
+		
 	}
 
 }
+
 
 
 //Done by Jesis
